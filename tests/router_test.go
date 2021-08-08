fef92e98eefe294d3d6422af6d22c423ff4ca9b3
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2021-08-08 23:14:05 +0200

    Added more test

--- tests/router_test.go
+++ tests/router_test.go
@@ -43,3 +43,39 @@ func TestGetDispatcher(t *testing.T) {
 		t.Error("Get on route /about not working")
 	}
 }
+
+func TestGetDispatcherWithRouteParams(t *testing.T) {
+	r := router.NewRouter()
+	m1 := &mockResponseWriter{}
+
+	r.Get("/about/{id}/andy/{word}/{sea}", testHandler)
+
+	request, _ := http.NewRequest(http.MethodGet, "/about/123/andy/string/blue", nil)
+	r.Dispatch(m1, request)
+
+	m2 := &mockResponseWriter{}
+	routeParameters := make(router.RouteParameters)
+	routeParameters["id"] = 123
+	routeParameters["word"] = "string"
+	routeParameters["sea"] = "blue"
+	testHandler(m2, request, routeParameters)
+	if m1.content != m2.content {
+		t.Error("Get on route /about/{id}/andy/{word}/{sea} not working")
+	}
+}
+
+func TestGetDispatcherWithRoot(t *testing.T) {
+	r := router.NewRouter()
+	m1 := &mockResponseWriter{}
+
+	r.Get("/", testHandler)
+
+	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	r.Dispatch(m1, request)
+
+	m2 := &mockResponseWriter{}
+	testHandler(m2, request, make(router.RouteParameters))
+	if m1.content != m2.content {
+		t.Error("Get on route / not working")
+	}
+}
